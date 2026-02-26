fc59abc01848bec5d8c1f19b5aeedb7780ea5cc3
ebiboy2 <ebiboy2@gmail.com>
2026-02-26 14:15:27 +0100

    Update Puppeteer configuration for headless mode

--- whatsapp-app/index.js
+++ whatsapp-app/index.js
@@ -153,15 +153,10 @@ const client = new Client({
     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
   },
   puppeteer: {
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-    headless: "chrome",
+    headless: true,
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--disable-gpu'
+      '--disable-setuid-sandbox'
     ]
   },
   authStrategy: new LocalAuth({
