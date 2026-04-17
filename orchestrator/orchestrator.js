a4fd49fc8db8d4c9b57cb8d2f6439256b03e1bc3
Emeti Etim <emetisuccess@gmail.com>
2026-04-17 13:51:20 +0100

    bot function upgrade

--- orchestrator/orchestrator.js
+++ orchestrator/orchestrator.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const registry = require("./registry");
+const registry = require("registry.js");
 const { exec } = require("child_process");
 
 const app = express();
